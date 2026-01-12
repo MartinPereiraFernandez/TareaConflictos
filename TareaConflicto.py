bf8b8048d044e480a9e056135595e63fb3df07c7
Pereira <mpereirafernandez@danielcastelao.org>
2026-01-12 14:27:56 +0100

    Commit Inicial

---
+++ TareaConflicto.py
@@ -0,0 +1,3 @@
+#Iniciamos la tarea de conflicto con un codigo simple, le preguntamos al usuario cuales son los números que quieren sumar.
+num1=int(input("Cual es el primer número que quieres sumar: "))
+num2=int(input("Cual es el segundo número que quieres sumar: "))
